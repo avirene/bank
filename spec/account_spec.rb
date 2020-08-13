d73104ccb76c96f003e721241090a5f2ad85b00f
Student <student@makerss-air.mynet>
2020-08-13 14:01:02 +0100

    Added 2 expectations that test making more than one deposit and withdrawal

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,20 +2,32 @@
 require "account.rb"
 
 describe Account do
+
+  let(:account) { Account.new }
+
   describe ".make_deposit" do
     it "allows to deposit money on the account" do
-      account = Account.new
       account.make_deposit(1000)
       expect(account.balance).to eq(1000.00)
+    end
+
+    it "allows to deposit money on the account more than once" do
+      3.times { account.make_deposit(1000) }
+      expect(account.balance).to eq(3000.00)
     end
   end
 
   describe ".make_withdrawal" do
     it "allows to withdraw money from the account" do
-      account = Account.new
       account.make_deposit(1000)
       account.make_withdrawal(500)
       expect(account.balance).to eq(500.00)
+    end
+
+    it "allows to withdraw money from the account more than once" do
+      account.make_deposit(1000)
+      3.times { account.make_withdrawal(500) }
+      expect(account.balance).to eq(-500.00)
     end
   end
 end
