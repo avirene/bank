2820bd5ff748f416562090587ef133813858972b
Student <student@makerss-air.mynet>
2020-08-15 14:45:20 +0100

    Changed the transaction instance variable into a local variable

--- lib/transaction_log.rb
+++ lib/transaction_log.rb
@@ -1,21 +1,20 @@
 class TransactionLog
   
-  attr_reader :transactions, :credit
+  attr_reader :transactions
 
   def initialize
     @transactions = []
-    @transaction = []
   end
 
   def log_deposit(account)
-    @transaction << Time.new << " " << "%0.2f" % [account.credit] << "%0.2f" % [account.balance]
-    @transactions << @transaction
-    @transaction = []
+    transaction = []
+    transaction << Time.new << " " << "%0.2f" % [account.credit] << "%0.2f" % [account.balance]
+    @transactions << transaction
   end
 
   def log_withdrawal(account)
-    @transaction << Time.new << "%0.2f" % [account.debit] << " " << "%0.2f" % [account.balance]
-    @transactions << @transaction
-    @transaction = []
+    transaction = []
+    transaction << Time.new << "%0.2f" % [account.debit] << " " << "%0.2f" % [account.balance]
+    @transactions << transaction
   end
 end
